562f9bfbe5ad05b11e4660806d8b4e1a73021b03
Wei Chen <tengwei1@gmail.com>
2017-08-18 16:26:51 -0400

    fixed recenter

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class ApiProvider {
-  private url: string = 'http://localhost:3000';
+  private url: string = 'http://127.0.0.1:3000';
 
   constructor(public http: Http) {
   }
